cee25a5d3bd975233e30e3d35d72dcb635c19826
Vaibhav Sachdeva <72242181+sachdevavaibhav@users.noreply.github.com>
2020-12-20 19:55:31 +0530

    Add files via upload

---
+++ Advent_of_Code_2020-Day_01/README.md
@@ -0,0 +1,3 @@
+# Advent_of_Code_2020-Day_01
+Solution to advent of code day_01
+day_01.txt contain the expense data.
---
+++ Advent_of_Code_2020-Day_01/day_01(part1).py
@@ -0,0 +1,27 @@
+# Numbers are: 1859,161
+# Result is 299299
+import random
+
+with open('day_01.txt') as file:
+    list_01 = []
+    for line in file:
+        stripped_line = line.rstrip()
+        list_01.append(stripped_line)
+
+numbers = list(map(lambda x: int(x), list_01))
+
+
+def get_2020():
+    while True:
+        num1 = random.choice(numbers)
+        num2 = random.choice(numbers)
+
+        sum = num1 + num2
+        if sum == 2020:
+            print(f'Numbers are: {num1},{num2}')
+            result = (num1 * num2)
+            print(f'Result is {result}')
+            break
+
+
+get_2020()
